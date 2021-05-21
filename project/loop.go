85d8c7e2c7aad7062bb7cf24d53cedef00efa1c3
Ваше Имя <raznomazov_vm@mail.ru>
2021-05-21 19:53:25 +0300

    switch was addicted

--- project/loop.go
+++ project/loop.go
@@ -10,5 +10,14 @@ func main() {
 		}else {
 		fmt.Println("Ай")
 		}
+		switch i {
+			case 0: fmt.Println("Zero")
+			case 1: fmt.Println("One")
+			case 2: fmt.Println("Two")
+			case 3: fmt.Println("Three")
+			case 4: fmt.Println("Four")
+			case 5: fmt.Println("Five")
+			default: fmt.Println("Unknown Number")
+		}
     }
 }
