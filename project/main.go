fd4213ac5ff494eb27e699c23f186b5e2f5e8c5b
Ваше Имя <raznomazov_vm@mail.ru>
2021-05-21 19:43:45 +0300

    variables were addicted

--- project/main.go
+++ project/main.go
@@ -8,4 +8,12 @@ func main() {
 	fmt.Println("Hello")
 	fmt.Println("1 + 1 = ", 1 + 1)
 	fmt.Println("1 + 1 =", 1.0 + 1.0)
+	fmt.Println(len("Hello World"))
+    fmt.Println("Hello World"[1])
+    fmt.Println("Hello " + "World")
+	fmt.Println(true && true)
+    fmt.Println(true && false)
+    fmt.Println(true || true)
+    fmt.Println(true || false)
+    fmt.Println(!true)
 }
---
+++ project/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x string = "Hello World"
+    fmt.Println(x)
+	var y string
+	y="Боня"
+	var z string
+	z=" ссыкуха"
+	fmt.Println(y+z)
+}
