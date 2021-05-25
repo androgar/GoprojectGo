81e3c5727954a259a60a923d79aea3df66f81b23
Ваше Имя <raznomazov_vm@mail.ru>
2021-05-25 14:46:41 +0300

    Some inputs

--- project/main.go
+++ project/main.go
@@ -8,21 +8,21 @@ import ("fmt"
 
 func main() {
      var a,b,c,d float64
-	 	 fmt.Println("a=")
+	 	 fmt.Println(" Please input a=")
 	 fmt.Scanf("%v \n", &a)
-	 	 fmt.Println("b=")
+	 	 fmt.Println("Please input b=")
 	 fmt.Scanf("%v \n", &b)
-	 	 fmt.Println("c=")
+	 	 fmt.Println("Please input c=")
 	 fmt.Scanf("%v \n", &c)
 	  	 d=b*b-4*a*c
 	 	 fmt.Println("\n")
 	 if d>0 {
-	   fmt.Println("koren № 1=", (-1*b-math.Pow(d,0.5))/(2*a))
-	   fmt.Println("koren № 2=", (-1*b+math.Pow(d,0.5))/(2*a))
+	   fmt.Println("root № 1=", (-1*b-math.Pow(d,0.5))/(2*a))
+	   fmt.Println("root № 2=", (-1*b+math.Pow(d,0.5))/(2*a))
 	 }else if d==0 {
-	   fmt.Println("koren =", (-1*b-math.Pow(d,0.5))/(2*a))
+	   fmt.Println("root =", (-1*b-math.Pow(d,0.5))/(2*a))
 	 }else if d<0 {
-	   fmt.Println("kornei net ")
+	   fmt.Println("There are not any roots ")
 	 }
 	     
 }
